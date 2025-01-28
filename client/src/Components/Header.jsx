c3e0300cc92be83f111693843d0baeefa79e31bd
Sanskrutitripathi <sanskrutitripathi@gmail.com>
2025-01-28 11:54:29 +0530

    profilepic

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -65,7 +65,7 @@ function Header() {
                 <img
                   src={imagePreview || Image}
                   alt="Profile"
-                  className="rounded-circle user-img"
+                  className="user-img"
                 />
               )}
             </a>
